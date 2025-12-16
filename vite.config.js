6872eff3e54328bd622cbf1658258935fe700ad5
jm-erazo <josemanuelmejiae511@gmail.com>
2025-12-16 00:37:07 -0500

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // IMPORTANTE: Cambia 'simulador-credito' por el nombre exacto de tu repositorio en GitHub
-  base: "/simulador-credito/",
-})
